a9e6e5eb38802774ed4f6a291eb13dd2968bfe2d
ahmednasser1911 <ahmednasser.fcis@gmail.com>
2022-05-21 23:04:29 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import CollapsibleTable from './MaterialTable';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='container m-5'>
+
+      <CollapsibleTable />
+      </div>
+    </div>
+  );
+}
+
+export default App;
